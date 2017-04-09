b655bdfe26197227f9576ed825b65ef05f7ebe4e
sammyteahan <sammyteahan@gmail.com>
2017-04-09 12:57:20 -0600

    HATEOAS

--- crops/serializers.py
+++ crops/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Crop, Category
 
 
 class CropSerializer(serializers.ModelSerializer):
 
+    links = serializers.SerializerMethodField()
+
     class Meta:
         model = Crop
         fields = ('name', 'pk', 'plant_start_date', 'plant_end_date',
-            'tier', 'transplant', 'direct_seed', 'category', 'notes', 'image')
+            'tier', 'transplant', 'direct_seed', 'category', 'notes',
+            'image', 'links')
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse('rest:crop-retrieve',
+                kwargs={'pk': obj.pk}, request=request)
+        }
 
 
 class CategorySerializer(serializers.ModelSerializer):
--- sfg/urls.py
+++ sfg/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rest/', include('crops.urls')),
+    url(r'^rest/', include('crops.urls', namespace='rest')),
 
     # for local development. nginx will serve these in staging/prod
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
